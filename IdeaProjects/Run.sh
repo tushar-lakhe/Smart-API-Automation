eae13bbc65e71baccb2d927feef20f2f94567f1e
tushar-lakhe <91410837+tushar-lakhe@users.noreply.github.com>
2022-12-22 20:07:23 +0530

    Update Run.sh

--- IdeaProjects/Run.sh
+++ IdeaProjects/Run.sh
@@ -4,19 +4,19 @@ echo "##############################################################"
 echo "##############################################################"
 echo "#################  Validating user inputs ####################"
 echo "##############################################################"
-python3 SmartAPIautomationframework/src/test/java/examples/users/pythonController/DataValidator.py
+python3 Smart-API-Automation/IdeaProjects/SmartAPIautomationframework/src/test/java/examples/users/pythonController/DataValidator.py
 echo "##############################################################"
 echo "############ User inputs validation Completed ################"
 echo "##############################################################"
 echo "##############################################################"
 echo "############### Generating Feature Files... ##################"
 echo "##############################################################"
-python3 SmartAPIautomationframework/src/test/java/examples/users/pythonController/FeatureGenerator.py
+python3 Smart-API-Automation/IdeaProjects/SmartAPIautomationframework/src/test/java/examples/users/pythonController/FeatureGenerator.py
 echo "##############################################################"
 echo "######### Feature Files Generated Successfully ###############"
 echo "##############################################################"
 echo "##############################################################"
 echo "############# Test Execution Begins... #######################"
 echo "##############################################################"
-python3 SmartAPIautomationframework/src/test/java/examples/users/pythonController/ReportGenerator.py
+python3 Smart-API-Automation/IdeaProjects/SmartAPIautomationframework/src/test/java/examples/users/pythonController/ReportGenerator.py
 read -n 1 -p prompt
