29550eac5d9905390560cff2181b2c510d4430c9
tushar-lakhe <91410837+tushar-lakhe@users.noreply.github.com>
2022-12-23 13:58:52 +0530

    Update Run.sh

--- IdeaProjects/Run.sh
+++ IdeaProjects/Run.sh
@@ -1,24 +1,3 @@
-echo "##############################################################"
-echo "###################  Initialising Test #######################"
-echo "##############################################################"
-echo "##############################################################"
-echo "#################  Validating user inputs ####################"
-echo "##############################################################"
-python3 SmartAPIautomationframework//src//test//java//examples//users//pythonController//DataValidator.py
-echo "##############################################################"
-echo "############ User inputs validation Completed ################"
-echo "##############################################################"
-echo "##############################################################"
-echo "############### Generating Feature Files... ##################"
-echo "##############################################################"
-cd /home/runner/work/Smart-API-Automation/Smart-API-Automation/IdeaProjects/SmartAPIautomationframework/src/test/java/examples/users/pythonController
-python3 FeatureGenerator.py
-echo "##############################################################"
-echo "######### Feature Files Generated Successfully ###############"
-echo "##############################################################"
-echo "##############################################################"
-echo "############# Test Execution Begins... #######################"
-echo "##############################################################"
-cd /home/runner/work/Smart-API-Automation/Smart-API-Automation/IdeaProjects/SmartAPIautomationframework/src/test/java/examples/users/pythonController
-python3 ReportGenerator.py
-read -n 1 -p prompt
+pwd
+ls
+python3 IdeaProjects/SmartAPIautomationframework//src//test//java//examples//users//pythonController//DataValidator.py
