ff901b0c8e7fa0a2f8a237e54a002650707f0501
tushar-lakhe <91410837+tushar-lakhe@users.noreply.github.com>
2022-12-23 13:45:08 +0530

    Update Run.sh

--- IdeaProjects/Run.sh
+++ IdeaProjects/Run.sh
@@ -4,19 +4,22 @@ echo "##############################################################"
 echo "##############################################################"
 echo "#################  Validating user inputs ####################"
 echo "##############################################################"
-python3 SmartAPIautomationframework/src/test/java/examples/users/pythonController/DataValidator.py
+cd IdeaProjects/SmartAPIautomationframework/src/test/java/examples/users/pythonController/
+python3 DataValidator.py
 echo "##############################################################"
 echo "############ User inputs validation Completed ################"
 echo "##############################################################"
 echo "##############################################################"
 echo "############### Generating Feature Files... ##################"
 echo "##############################################################"
-python3 SmartAPIautomationframework/src/test/java/examples/users/pythonController/FeatureGenerator.py
+cd IdeaProjects/SmartAPIautomationframework/src/test/java/examples/users/pythonController/
+python3 FeatureGenerator.py
 echo "##############################################################"
 echo "######### Feature Files Generated Successfully ###############"
 echo "##############################################################"
 echo "##############################################################"
 echo "############# Test Execution Begins... #######################"
 echo "##############################################################"
-python3 SmartAPIautomationframework/src/test/java/examples/users/pythonController/ReportGenerator.py
+cd IdeaProjects/SmartAPIautomationframework/src/test/java/examples/users/pythonController/
+python3 ReportGenerator.py
 read -n 1 -p prompt
